4cd3ed49285bb15d4dff5e716570a362ef582ab8
Arjun Kaondal <arjunk7427@gmail.com>
2026-09-13 15:11:05 +0000

    created springboot application and dockerized it

--- starter/src/main/java/com/neueda/leap/Main.java
+++
@@ -1,9 +0,0 @@
-package com.neueda.leap;
-
-public class Main {
-    public static void main(String[] args) {
-        // TODO: replace <team-name> with your team's actual name
-        System.out.println("Hello world from localhost's Sprint 1 project skeleton");
-        System.out.println("Testing jenkings pr");
-    }
-}
