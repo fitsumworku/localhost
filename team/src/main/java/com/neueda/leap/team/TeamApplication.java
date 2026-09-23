614de9a4fad80c14386ce1130fcc76353ab119c9
Sai Mummadisingu <saikarthikvm@gmail.com>
2026-09-23 02:25:59 +0000

    Configured database connection in application.properties

--- team/src/main/java/com/neueda/leap/team/TeamApplication.java
+++ team/src/main/java/com/neueda/leap/team/TeamApplication.java
@@ -3,7 +3,7 @@ package com.neueda.leap.team;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication()
 public class TeamApplication {
 
 	public static void main(String[] args) {
