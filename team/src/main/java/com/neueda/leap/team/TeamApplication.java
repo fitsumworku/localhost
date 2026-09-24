a32ef28457bfaf1121d52f174059b04176c26059
Sai Mummadisingu <saikarthikvm@gmail.com>
2026-09-24 22:46:47 +0000

    Testing again if spring connects with db

--- team/src/main/java/com/neueda/leap/team/TeamApplication.java
+++ team/src/main/java/com/neueda/leap/team/TeamApplication.java
@@ -11,18 +11,19 @@ import java.sql.SQLException;
 @SpringBootApplication()
 public class TeamApplication {
 
-	static String url = System.getenv("SPRING_DATASOURCE_URL");
-	static String username = System.getenv("SPRING_DATASOURCE_USERNAME");
-	static String password = System.getenv("SPRING_DATASOURCE_PASSWORD");
+//	static String url = System.getenv("SPRING_DATASOURCE_URL");
+//	static String username = System.getenv("SPRING_DATASOURCE_USERNAME");
+//	static String password = System.getenv("SPRING_DATASOURCE_PASSWORD");
 
 	public static void main(String[] args) {
-		try {
-			Connection conn = DriverManager.getConnection(url, username, password);
-            System.out.println("Connected to database successfully ITS WORKING");
-			conn.close();
-		} catch (SQLException e) {
-			throw new RuntimeException(e.getMessage());
-		}
+//		try {
+//			Connection conn = DriverManager.getConnection(url, username, password);
+//            System.out.println("Connected to database successfully ITS WORKING");
+//			conn.close();
+//		} catch (SQLException e) {
+//			throw new RuntimeException(e.getMessage());
+//		}\
+		SpringApplication.run(TeamApplication.class, args);
 
 	}
 
