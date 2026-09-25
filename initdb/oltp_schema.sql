52f357a551e13c027b339f018dae220b6ff5b54e
Jacob <jtahmoush1287@gmail.com>
2026-09-25 08:48:42 -0400

    resolved merge conflict

--- initdb/oltp_schema.sql
+++ initdb/oltp_schema.sql
@@ -127,6 +127,7 @@ CREATE TABLE Executions (
 CREATE TABLE Trades (
     Trade_ID BIGSERIAL PRIMARY KEY,
     Execution_ID BIGINT NOT NULL,
+    Security_ID BIGINT NOT NULL,
     Trade_Price NUMERIC(18,4) NOT NULL,
     Shares NUMERIC(18,4) NOT NULL,
     Trade_Status VARCHAR(18) NOT NULL,
